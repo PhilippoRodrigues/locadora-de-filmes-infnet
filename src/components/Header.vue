181a514cfd5e2086855581e9907506a225519f0b
Thaís do Nascimento Viana <thais.viana@prof.infnet.edu.br>
2020-03-06 18:08:57 -0300

    filme

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,39 +1,14 @@
 <template>
   <div id="header" href="#">
-         <b-navbar toggleable="lg" type="dark" variant="dark">
-    <router-link
-           tag="h5"
-           class="card-title"
-           :to="{ name: 'home' }"
-         ><b-navbar-brand >Locadora de Filmes</b-navbar-brand>
-         </router-link>
-    
-
-    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
-    <b-collapse id="nav-collapse" is-nav>
-     
-
-      <!-- Right aligned nav items -->
-      <b-navbar-nav class="ml-auto">
-        
-
-
-      </b-navbar-nav>
-    </b-collapse>
-  </b-navbar>
+     <b-navbar toggleable="lg" type="dark" variant="dark">
+       <b-navbar-brand :to="{ name: 'home' }" >Locadora de Filmes</b-navbar-brand>
+       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+     </b-navbar>
   </div>
 </template>
 
 <script>
-
 export default {
-
   name: 'Header',
-
 }
 </script>
-
-<style>
-
-</style>
